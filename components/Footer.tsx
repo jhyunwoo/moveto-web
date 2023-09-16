eed1105be82670a4e8e30923b3e6656ba12d23c6
jhyunwoo <jhyunwoo0228@gmail.com>
2023-09-16 12:39:19 +0900

    add develop note on footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,11 +5,16 @@ export default function Footer() {
     <div className="itmes-start flex flex-col justify-center space-y-2 bg-slate-100 p-4 pb-20 text-xs text-slate-700 dark:bg-slate-900 dark:text-slate-200 sm:pb-4">
       <div>Copyright 2023. Moveto Team. all rights reserved.</div>
       <div>Admin: jhyunwoo0228@gmail.com</div>
+      <Link href={"https://github.com/jhyunwoo"} className="hover:underline">
+        Github
+      </Link>
       <Link
-        href={"https://github.com/jhyunwoo/moveto-web"}
+        href={
+          "https://hyunwoo0228.notion.site/hyunwoo0228/Moveto-ca98b2d6253a44e59bebe837105d5998        "
+        }
         className="hover:underline"
       >
-        Github
+        개발 과정
       </Link>
       <Link href={"/privacy"} className="hover:underline">
         개인정보처리방침
