a8078870c17a79e36a8add4c871b383020478501
jhyunwoo <jhyunwoo0228@gmail.com>
2024-06-09 14:09:48 +0900

    add system noti on home page

--- app/page.tsx
+++ app/page.tsx
@@ -56,8 +56,11 @@ export default function Home() {
   return (
     <div className=" flex min-h-screen w-full flex-col items-center justify-center p-8 dark:text-white sm:pt-16">
       <div className="flex w-full max-w-xl flex-col items-center justify-center">
-        <div className="mr-auto pb-1 pl-2 text-xl font-semibold dark:text-white">
-          코드
+        <div className={'w-full flex items-center justify-start'}>
+          <div className="mr-auto pb-1 pl-2 text-xl font-semibold dark:text-white">
+            코드
+          </div>
+          <div className={'text-sm'}>시스템 점검: 2024년 6월 22일 ~ 23일</div>
         </div>
         <SearchFile ip={ip} />
       </div>
